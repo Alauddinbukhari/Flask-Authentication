8ea0d4f5c7f6b9e18d79049f3bf34076c17d16fd
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2025-12-05 22:44:35 -0700

    added some routes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,request, render_template, redirect, url_for, session, flash
 
 app= Flask(__name__)
 
@@ -10,9 +10,21 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        pass
     return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return "<p>signup</p>"
+
+
+@app.route("/logor_password")
+def forgot_password():
+    return "<p>forgot password</p>"
 
 
 if __name__=="__main__":
